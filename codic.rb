3ca44b9c13e5de5c3957a0f0cb96b710ddc1441a
Sa2Knight <shingo.sasaki.0529@gmail.com>
2017-05-14 18:18:50 +0900

    シェルから呼び出しやすいように修正

--- codic.rb
+++ codic.rb
@@ -3,7 +3,6 @@ require 'uri'
 require 'json'
 
 class Codic
-
   @@API_URL = "https://api.codic.jp/v1/engine/translate.json"
 
   # コンストラクタ
@@ -15,11 +14,25 @@ class Codic
   # 翻訳する
   # 指定した日本語に基いて、Codicを用いて翻訳した結果を戻す
   def translate(text, casing = '')
+    casing = convertCasing(casing)
     result = request(@@API_URL, {:text => text, :casing => casing})
-    result[0]["translated_text"] if result.count > 0 && result[0].include?("translated_text")
+    result[0]["translated_text"] if result.class == Array && result[0].include?("translated_text")
   end
 
   private
+    # casingをAPI用に変換する
+    def convertCasing(casing)
+      casingList = {
+        'c' => 'camel',
+        'p' => 'pascal',
+        'l' => 'lower underscore',
+        'u' => 'upper underscore',
+        'h' => 'hyphen',
+        'n' => 'n'
+      }
+      new_casing = casingList[casing]
+      new_casing ? new_casing : ''
+    end
     # HTTPリクエストを送信し、レスポンスJSONを戻す
     def request(url, params = {})
       uri = URI.parse(appendQueryString(url, params))
@@ -36,5 +49,10 @@ class Codic
       url += "?#{query_string}" if query_string
       return url
     end
-
 end
+
+# コマンドライン引数を元に翻訳を実行
+codic = Codic.new
+word = ARGV[0] || exit
+casing = ARGV[1] || ''
+puts codic.translate(word, casing)
